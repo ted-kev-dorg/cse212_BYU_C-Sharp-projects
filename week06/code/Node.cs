89aaab9d385f4da294c2cce4e5d81df790040908
ted-kev-dorg <kevin.tetede@gmail.com>
2025-08-11 22:25:43 -0400

    Week06  Prove assignement

--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -12,8 +12,13 @@ public class Node
     public void Insert(int value)
     {
         // TODO Start Problem 1
+        if (value == Data)
+        {
+            // Do nothing — duplicate
+            return;
+        }
 
-        if (value < Data)
+        else if (value < Data)
         {
             // Insert to the left
             if (Left is null)
@@ -34,12 +39,19 @@ public class Node
     public bool Contains(int value)
     {
         // TODO Start Problem 2
-        return false;
+        if (value == Data)
+            return true;
+        else if (value < Data)
+            return Left != null && Left.Contains(value);
+        else // value > Data
+            return Right != null && Right.Contains(value);
     }
 
     public int GetHeight()
     {
         // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left?.GetHeight() ?? 0;
+        int rightHeight = Right?.GetHeight() ?? 0;
+        return Math.Max(leftHeight, rightHeight) + 1;
     }
 }
